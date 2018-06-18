22671d78e6610e3fcd151fb31545316fcdc72502
maria laura lucanera <mllucanera@gmail.com>
2018-06-18 14:59:10 -0300

    boat model created with association to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_18_172415) do
+ActiveRecord::Schema.define(version: 2018_06_18_175612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boats", force: :cascade do |t|
+    t.integer "guests"
+    t.string "type"
+    t.string "address"
+    t.integer "price_day"
+    t.text "description"
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +45,5 @@ ActiveRecord::Schema.define(version: 2018_06_18_172415) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "boats", "users"
 end
