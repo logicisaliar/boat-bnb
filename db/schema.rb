94a113c3820314c567a23b236bbf80ddc5652bfc
maria laura lucanera <mllucanera@gmail.com>
2018-06-18 17:16:04 -0300

    Cleaning the change on the schema after pull

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_06_18_174545) do
+ActiveRecord::Schema.define(version: 2018_06_18_175612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
