14c9e1f02c44a1a789de07ad7cc4a1c8915abe47
Jash Shah <jash.shah@live.com>
2018-06-19 14:05:32 -0300

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :boats, only: [:index, :show] do
+    resources :bookings, only: [:new, :create, :edit, :update]
+  end
+  resources :bookings, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :host do
+    resources :boats do
+      resources :bookings
+    end
+  end
 end
+
