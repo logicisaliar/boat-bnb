d2579583e5607a871c14893c3a0fb9cc411559c3
maria laura lucanera <mllucanera@gmail.com>
2018-06-19 17:11:25 -0300

    photo uploaded and working

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -44,7 +44,7 @@ class BoatsController < ApplicationController
   private
 
   def boat_params
-    params.require(:boat).permit(:name, :guests, :boat_type, :address, :price_day, :description)
+    params.require(:boat).permit(:name, :guests, :boat_type, :address, :price_day, :description, :photo_cache)
   end
 
   def set_boat
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,6 @@ class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :users, through: :bookings
+
+  mount_uploader :photo, PhotoUploader
 end
