8cab9ed82d98baf4b11ae358e50b440ef2e18fd0
joti sempe <josefinasempe@gmail.com>
2018-06-20 13:26:04 -0300

    changed all the redirection routes in actions. created module Host in Boats. 2 boat controllers (host & user)

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,57 +1,19 @@
 class BoatsController < ApplicationController
-  before_action :set_boat, only: [:show, :edit, :update, :destroy]
 
   def index
     # list the boats of the current user
     # @boats = Boat.where(user_id: current_user.id)
-    @boats = current_user.owned_boats
-  end
-
-  def show_user
+    @boats = Boat.all
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @boat.update(boat_params)
-      redirect_to boat_path(@boat)
-    else
-      render :edit
-    end
-  end
-
-  def new
-    @boat = Boat.new
-    @boat.user = current_user
-  end
-
-  def create
-    @boat = Boat.new(boat_params)
-    @boat.user = current_user
-    if @boat.save
-      redirect_to boats_path
-    else
-      render :new
-    end
-  end
-
-  def destroy
-    @boat.destroy
-    redirect_to boats_path
+    @boat = Boat.find(params[:id])
   end
 
   private
 
   def boat_params
     params.require(:boat).permit(:name, :guests, :boat_type, :address, :price_day, :description, :photo_cache)
-  end
-
-  def set_boat
-    @boat = Boat.find(params[:id])
   end
 end
 
