833be97233528872b7c7a8da8dc96fb573da5969
joti sempe <josefinasempe@gmail.com>
2018-06-21 17:35:13 -0300

    home style almost there

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def bg_style(boat)
+    default = "background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.2))"
+    if boat.photo.present?
+      bg_url = ", url('#{cl_image_path(b.photo, height: 300, width: 400, crop: :fill)}')"
+    else
+      bg_url = nil
+    end
+    return "%{default}#{bg_url}"
+  end
 end
