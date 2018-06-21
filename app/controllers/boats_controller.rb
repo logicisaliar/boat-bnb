a0cea065d2412e553624273d6d7bd9c2424c700b
Jash Shah <jash.shah@live.com>
2018-06-21 19:37:27 -0300

    Link works. But pop-up needs to be look much better. Thanks Joti.

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,18 +1,17 @@
 class BoatsController < ApplicationController
-skip_before_action :authenticate_user!, only: [:show, :index, :search_by_city]
+  skip_before_action :authenticate_user!, only: [:show, :index, :search_by_city]
 
 
   def search_by_city
     # a = render "../views/pages/info_window"
-
     address = params[:city_name]
     @boats = Boat.near(address, 20)
     @markers = @boats.map do |boat|
-      # authorize boat
+      authorize boat
       {
         lat: boat.latitude,
         lng: boat.longitude,
-        infoWindow: { content: "a" }
+        infoWindow: { content: render_to_string(partial: "/shared/mapbox", locals: { boat: boat }) }
       }
     end
   end
